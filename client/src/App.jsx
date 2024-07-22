d9f321d4fb0e1bf88972280035c8b081f523b01f
mihailhr <mishohristov3@gmail.com>
2024-07-22 12:20:04 +0300

    Added a filler component and a footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 
+import Content from "./Components/Content"
+import Footer from "./Components/Footer"
 import Navbar from "./Components/Navbar"
 import "./styles.css"
 export default function App(){
   return(
     <div>
       <Navbar/>
-    
-      
+      <Content/>
+      <Footer/>
     </div>
   )
 }
---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer>
+            <p>&copy; GamesShop.bg 2024; All rights reserved</p>
+        </footer>
+    )
+}
