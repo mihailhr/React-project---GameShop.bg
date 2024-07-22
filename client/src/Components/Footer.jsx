e09e797e7a724cff1d7b73e5df6b361d447afa26
mihailhr <mishohristov3@gmail.com>
2024-07-22 14:22:33 +0300

    Added a home component

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,7 +1,11 @@
+import {FaGithub} from "react-icons/fa"
+import {Link} from "react-router-dom"
+
 export default function Footer(){
     return(
         <footer>
-            <p>&copy; GamesShop.bg 2024; All rights reserved</p>
+            <p>&copy; GamesShop.bg 2024. All rights reserved.</p>  
+               <div className="git"><Link to={"https://github.com/mihailhr/React-project---GameShop.bg"}><FaGithub/></Link></div> 
         </footer>
     )
 }
--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,7 +1,31 @@
+import {Link} from "react-router-dom"
+
 export default function Home(){
     return(
         <div className="home">
-            <h1>home</h1>
+                <article className="videoArticle">
+                    <div className="homeVideo">
+                        <video   autoPlay loop muted>
+                            <source src="/videos/Virtual Variety Game Fun Presentation in Black Blue Orange Brutalist Cyberpunk Style.mp4" type="video/mp4" />
+        
+                        </video>
+                    </div>
+                </article>
+                <section className="fields">
+                    <div className="buy">
+                        <Link to={"/catalog"}><h3>Find and buy your favorite games</h3></Link>
+                        <img src="/game.jpg" alt="" />
+                    </div>
+                    <div className="sell">
+                    <Link to={"/createNewGame"}><h3>Sell games that you are not going to enjoy anymore</h3></Link>
+                    <img src="/money.jpg" alt="" />
+                    </div>
+                    <div className="gamer">
+                        <h3>Become one of us</h3>
+                        <img src="/join.jpg" alt="" />
+                    </div>
+                </section>
         </div>
+        
     )
 }
